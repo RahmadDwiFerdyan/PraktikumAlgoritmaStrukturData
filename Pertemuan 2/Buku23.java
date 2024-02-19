4a7b5fa1c37127ebefc4747858326d1fec570248
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2024-02-19 10:38:28 +0700

    membuat class yang berisikan atribut dan method

---
+++ Pertemuan 2/Buku23.java
@@ -0,0 +1,25 @@
+public class Buku23 {
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilanInformasi(){
+        System.out.println("Judul           : " + judul);
+        System.out.println("Pengarang       : " + pengarang);
+        System.out.println("Jumlah halaman  : " + halaman);
+        System.out.println("Sisa stok       : " + stok);
+        System.out.println("Harga           : Rp" + harga);
+    }
+
+    void terjual (int jml){
+        if (stok>0){
+          stok -= jml;  
+        }
+    }
+    void restock (int jml){
+        stok += jml;
+    }
+    void gantiHarga (int hrg){
+        harga = hrg;
+    }
+
+}
