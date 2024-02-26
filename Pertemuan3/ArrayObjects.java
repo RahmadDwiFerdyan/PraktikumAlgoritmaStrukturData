53ced38ca947234f106215a35fa4b1bd4ba3219c
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2024-02-26 10:12:41 +0700

    mengisi array dengan inputan dan looping

--- Pertemuan3/ArrayObjects.java
+++ Pertemuan3/ArrayObjects.java
@@ -1,23 +1,25 @@
 package PraktikumAlgoritmaStrukturData.Pertemuan3;
-
+import java.util.Scanner;
 public class ArrayObjects {
     public static void main(String[] args) {
         PersegiPanjang[] ppArray = new PersegiPanjang[3];
 
-        ppArray[0] = new PersegiPanjang();
-        ppArray[0].panjang = 110;
-        ppArray[0].lebar = 30;
+        Scanner sc = new Scanner(System.in);
 
-        ppArray[1] = new PersegiPanjang();
-        ppArray[1].panjang = 80;
-        ppArray[1].lebar = 40;
+        for (int i=0; i<3; i++){
+            ppArray[i] = new PersegiPanjang();
+            System.out.println("Persegi panjang ke-"+i);
+            System.out.print("Masukkan panjang: ");
+            ppArray[i].panjang = sc.nextInt();
+            System.out.print("Masukkan lebar: ");
+            ppArray[i].lebar = sc.nextInt();
 
-        ppArray[2] = new PersegiPanjang();
-        ppArray[2].panjang = 100;
-        ppArray[2].lebar = 20;
+        }
 
-        System.out.println("Persegi Panjang ke-0, panjang: " +ppArray[0].panjang + ", lebar: " + ppArray[0].lebar);
-        System.out.println("Persegi Panjang ke-1, panjang: " +ppArray[1].panjang + ", lebar: " + ppArray[1].lebar);
-        System.out.println("Persegi Panjang ke-2, panjang: " +ppArray[2].panjang + ", lebar: " + ppArray[2].lebar);
+        for (int i=0; i<3; i++){
+            System.out.println("Persegi panjang ke-"+i);
+            System.out.println("Panjang: "+ppArray[i].panjang + ", lebar: "+ppArray[i].lebar);
+        }
+        
     }
 }
