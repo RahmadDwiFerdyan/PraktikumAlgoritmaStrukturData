eeb29f09ef494d4c70dd7ce9a0f4c9368612e9fc
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2024-03-22 21:32:43 +0700

    4.5 Latihan Praktikum - Mencari nilai teritinggi, terendah, dan ratarata

---
+++ Pertemuan5/MainShowroom.java
@@ -0,0 +1,23 @@
+package PraktikumAlgoritmaStrukturData.Pertemuan5;
+
+public class MainShowroom {
+    public static void main(String[] args) {
+        Showroom[] mobil = {
+                new Showroom("BMW M2", "Coupe", 2016, 6816, 728),
+                new Showroom("Ford", "Fiesta ST", 2014, 3921, 575),
+                new Showroom("Nissan", "370Z", 2009, 4360, 657),
+                new Showroom("Subaru", "BRZ", 2014, 4058, 609),
+                new Showroom("Subaru", "Impreza WRX STI", 2013, 6255, 703),
+                new Showroom("Toyota", "AE86 Trueno", 1986, 3700, 553),
+                new Showroom("Toyota", "86/GT86", 2014, 4180, 609),
+                new Showroom("Volkswagen", "Golf GTI", 2014, 4180, 631),
+        };
+
+        Showroom mbl = new Showroom();
+        System.out.println("=======================================");
+        System.out.println("Top acceleration tertinggi  : " + mbl.accelTertinggi(mobil, 0, mobil.length - 1));
+        System.out.println("Top acceleration terendah   : " + mbl.accelerationTerendah(mobil, 0, mobil.length - 1));
+        System.out.printf("Rata-rata top power         : %.2f", mbl.AvgPowers(mobil));
+
+    }
+}
---
+++ Pertemuan5/Showroom.java
@@ -0,0 +1,61 @@
+package PraktikumAlgoritmaStrukturData.Pertemuan5;
+
+public class Showroom {
+    public String merk, tipe;
+    public int tahun, top_accel, top_power;
+
+    Showroom(String merk, String tipe, int tahun, int top_accel, int top_power) {
+        this.merk = merk;
+        this.tipe = tipe;
+        this.tahun = tahun;
+        this.top_accel = top_accel;
+        this.top_power = top_power;
+    }
+
+    public Showroom() {
+
+    }
+
+    int accelTertinggi(Showroom[] mobil, int left, int right) {
+        if (left == right) {
+            return mobil[left].top_accel;
+        } else if (left < right) {
+            int mid = (left + right) / 2;
+            int leftmax = accelTertinggi(mobil, left, mid);
+            int rightmax = accelTertinggi(mobil, mid + 1, right);
+
+            if (leftmax > rightmax) {
+                return leftmax;
+            } else {
+                return rightmax;
+            }
+        }
+        return -1;
+    }
+
+    int accelerationTerendah(Showroom[] mobil, int left, int right) {
+        if (left == right) {
+            return mobil[left].top_accel;
+        } else if (left < right) {
+            int mid = (left + right) / 2;
+            int leftmin = accelerationTerendah(mobil, left, mid);
+            int rightmin = accelerationTerendah(mobil, mid + 1, right);
+
+            if (leftmin < rightmin) {
+                return leftmin;
+            } else {
+                return rightmin;
+            }
+        }
+        return -1;
+    }
+
+    double AvgPowers (Showroom [] mobil){
+        double total = 0;
+        for (Showroom mb : mobil) {
+            total += mb.top_power;
+        }
+        return total/mobil.length;
+    }
+
+}
