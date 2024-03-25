04c38f60e0b1b16c3cf1aa7e73ce729c3d788894
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2024-03-25 10:02:56 +0700

    Membuat algoritma bubble sort

---
+++ Pertemuan6/DaftarMahasiswaBerprestasi.java
@@ -0,0 +1,37 @@
+package PraktikumAlgoritmaStrukturData.Pertemuan6;
+
+public class DaftarMahasiswaBerprestasi {
+    Mahasiswa listMhs[] = new Mahasiswa[5];
+    int idx;
+
+    void tambah(Mahasiswa m) {
+        if (idx < listMhs.length) {
+            listMhs[idx] = m;
+            idx++;
+        } else {
+            System.out.println("Data sudah penuh!");
+        }
+    }
+
+    void tampil() {
+        for (Mahasiswa m : listMhs) {
+            m.tampil();
+            System.out.println("---------------------------");
+            ;
+        }
+    }
+
+    void bubbleSort() {
+        for (int i = 0; i < listMhs.length - 1; i++) {
+            for (int j = 1; j < listMhs.length - i; j++) {
+                if (listMhs[j].ipk > listMhs[j - 1].ipk) {
+                    Mahasiswa tmp = listMhs[j];
+                    listMhs[j] = listMhs[j - 1];
+                    listMhs[j - 1] = tmp;
+                }
+            }
+        }
+    }
+
+    
+}
---
+++ Pertemuan6/Mahasiswa.java
@@ -0,0 +1,22 @@
+package PraktikumAlgoritmaStrukturData.Pertemuan6;
+public class Mahasiswa {
+
+    String nama;
+    int thnMasuk, umur;
+    double ipk;
+
+    Mahasiswa(String n, int t, int u, double i){
+        nama = n;
+        thnMasuk = t;
+        umur = u;
+        ipk = i;    
+    }
+
+    void tampil(){
+        System.out.println("Nama        : " + nama);
+        System.out.println("Tahun masuk : " + thnMasuk);
+        System.out.println("Umur        : " + umur);
+        System.out.println("IPK         : " + ipk);
+    }
+    
+}
