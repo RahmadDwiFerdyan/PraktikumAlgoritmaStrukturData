5087206b9da33093a2a68cc18214872b1ed28803
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2024-04-20 09:35:37 +0700

    2.1 Percobaan 1: Penyimpanan Tumpukan Barang dalam Gudang

---
+++ Pertemuan8/Barang23.java
@@ -0,0 +1,13 @@
+package PraktikumAlgoritmaStrukturData.Pertemuan8;
+
+public class Barang23 {
+    int kode;
+    String nama, kategori;
+
+    public Barang23(int kode, String nama, String kategori) {
+        this.kode = kode;
+        this.nama = nama;
+        this.kategori = kategori;
+    }
+
+}
---
+++ Pertemuan8/Gudang23.java
@@ -0,0 +1,62 @@
+package PraktikumAlgoritmaStrukturData.Pertemuan8;
+
+public class Gudang23 {
+    Barang23[] tumpukan;
+    int size, top;
+
+    public Gudang23(int kapasitas) {
+        size = kapasitas;
+        tumpukan = new Barang23[size];
+        top = -1;// 0
+    }
+
+    public boolean cekKosong() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean cekPenuh() {
+        if (top == size - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void tambahBarang(Barang23 brg) {
+        if (!cekPenuh()) {
+            top++;
+            tumpukan[top] = brg;
+            System.out.println("Barang " + brg.nama + " berhasil ditambahkan ke Gudang");
+        } else {
+            System.out.println("Gagal! Tumpukan barang di Gudang sudah penuh!");
+        }
+    }
+
+    public Barang23 ambilBarang() {
+        if (!cekKosong()) {
+            Barang23 delete = tumpukan[top];
+            top--;
+            System.out.println("Barang " + delete.nama + " diambil dari Gudang.");
+            return delete;
+        } else {
+            System.out.println("Tumpukan barang kosong.");
+            return null;
+        }
+    }
+
+    public void tampilkanBarang() {
+        if (!cekKosong()) {
+            System.out.println("Rincian tumpukan barang di Gudang:");
+            for (int i = top; i >= 0; i--) {
+                System.out.printf("Kode %d: %s (Kategori %s)\n", tumpukan[i].kode, tumpukan[i].nama,
+                        tumpukan[i].kategori);
+            }
+        } else {
+            System.out.println("Tumpukan barang kosong.");
+        }
+    }
+}
