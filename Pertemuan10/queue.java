c762356ea0e65c1076440957841bcc37cef3d866
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2024-05-05 23:41:02 +0700

    10.2 Praktikum 1 (Modifikasi End Program ketika overflow/ underflow)

--- Pertemuan10/queue.java
+++ Pertemuan10/queue.java
@@ -65,6 +65,7 @@ public class queue {
     public void enqueue(int dt) {
         if (isFull()) {
             System.out.println("Queue sudah penuh");
+            System.exit(0);
         } else {
             if (isEmpty()) {
                 front = rear = 0;
@@ -84,6 +85,7 @@ public class queue {
         int dt = 0;
         if (isEmpty()) {
             System.out.println("Queue masih kosong");
+            System.exit(0);
         } else {
             dt = data[front];
             size--;
