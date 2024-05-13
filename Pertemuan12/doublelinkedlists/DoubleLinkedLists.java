f6cfde9b027c9605ace392d07a7cfa8aec28df24
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2024-05-13 11:34:15 +0700

    Praktikum2 - menambahkan method remove

--- Pertemuan12/doublelinkedlists/DoubleLinkedLists.java
+++ Pertemuan12/doublelinkedlists/DoubleLinkedLists.java
@@ -4,17 +4,17 @@ public class DoubleLinkedLists {
     Node head;
     int size;
 
-    public DoubleLinkedLists(){
+    public DoubleLinkedLists() {
         head = null;
         size = 0;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return head == null;
     }
 
-    public void addFirst (int item){
-        if (isEmpty()){
+    public void addFirst(int item) {
+        if (isEmpty()) {
             head = new Node(null, item, null);
         } else {
             Node newNode = new Node(null, item, head);
@@ -24,12 +24,12 @@ public class DoubleLinkedLists {
         size++;
     }
 
-    public void addLast (int item){
-        if(isEmpty()){
+    public void addLast(int item) {
+        if (isEmpty()) {
             addFirst(item);
         } else {
             Node current = head;
-            while (current.next != null){
+            while (current.next != null) {
                 current = current.next;
             }
             Node newNode = new Node(null, item, head);
@@ -39,20 +39,20 @@ public class DoubleLinkedLists {
         size++;
     }
 
-    public void add (int item, int index) throws Exception {
-        if(isEmpty()){
+    public void add(int item, int index) throws Exception {
+        if (isEmpty()) {
             addFirst(item);
-        } else if (index < 0 || index > size){
-            throw  new Exception("Nilai index di luar batas");
+        } else if (index < 0 || index > size) {
+            throw new Exception("Nilai index di luar batas");
         } else {
             Node current = head;
             int i = 0;
-            while (i < index){
+            while (i < index) {
                 current = current.next;
                 i++;
             }
-            if (current.prev == null){
-                Node newNode = new Node (null, item, current);
+            if (current.prev == null) {
+                Node newNode = new Node(null, item, current);
                 current.prev = newNode;
                 head = newNode;
             } else {
@@ -66,19 +66,19 @@ public class DoubleLinkedLists {
         size++;
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public void clear(){
+    public void clear() {
         head = null;
         size = 0;
     }
 
-    public void print(){
-        if(!isEmpty()){
+    public void print() {
+        if (!isEmpty()) {
             Node tmp = head;
-            while (tmp != null){
+            while (tmp != null) {
                 System.out.print(tmp.data + "\t");
                 tmp = tmp.next;
             }
@@ -87,5 +87,61 @@ public class DoubleLinkedLists {
             System.out.println("Linked Lists kosong!");
         }
     }
+
+    public void removeFirst() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked List masih kosong, tidak dapat dihapus!");
+        } else if (size == 1) {
+            removeLast();
+        } else {
+            head = head.next;
+            head.prev = null;
+            size--;
+        }
+    }
+
+    public void removeLast() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked List masih kosong, tidak dapat dihapus!");
+        } else if (head.next == null) {
+            head = null;
+            size--;
+            return;
+        }
+        Node current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+        size--;
+    }
+
+    public void remove(int index) throws Exception {
+        if(isEmpty() || index >= size){
+            throw new Exception("Nilai indeks diluar batas");
+        } else if (index == 0){
+            removeFirst();
+        } else {
+            Node current = head;
+            int i = 0;
+            while (i < index){
+                current = current.next;
+                i++;
+            }
+            if (current.next == null){
+                current.prev.next = null;
+            } else if (current.prev == null){
+                current = current.next;
+                current.prev = null;
+                head = current;
+            } else {
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+            }
+            size--;
+        }
+    }
+
+    
 
 }
