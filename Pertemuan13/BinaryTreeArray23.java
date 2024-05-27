b2bcfdb58c6b0b7a3c784e3113dbfea94caf1002
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2024-05-27 13:20:53 +0700

    Modify class (tambah method add, traversePreOrder, dan traversePostOrder)

--- Pertemuan13/BinaryTreeArray23.java
+++ Pertemuan13/BinaryTreeArray23.java
@@ -4,17 +4,43 @@ public class BinaryTreeArray23 {
     int[] data;
     int idxLast;
 
-    public BinaryTreeArray23(){
+    public BinaryTreeArray23() {
         data = new int[10];
+        idxLast = -1;
     }
 
-    void populateData(int[] data, int idxLast){
+    void populateData(int[] data, int idxLast) {
         this.data = data;
         this.idxLast = idxLast;
     }
 
-    void traverseInOrder (int idxStart){
-        if(idxStart <= idxLast){
+    void add(int nilai) {
+        if (idxLast < data.length - 1) {
+            idxLast++;
+            data[idxLast] = nilai;
+        } else {
+            System.out.println("Tree sudah penuh!");
+        }
+    }
+
+    void traversePreOrder(int idxStart) {
+        if (idxStart <= idxLast) {
+            System.out.print(data[idxStart] + " ");
+            traversePreOrder(2* idxStart+1);
+            traversePreOrder(2*idxStart+2);
+        }
+    }
+
+    void traversePostOrder(int idxStart) {
+        if (idxStart <= idxLast) {
+            traversePostOrder(2*idxStart+1);
+            traversePostOrder(2*idxStart+2);
+            System.out.print(data[idxStart] + " ");
+        }
+    }
+
+    void traverseInOrder(int idxStart) {
+        if (idxStart <= idxLast) {
             traverseInOrder(2*idxStart+1);
             System.out.print(data[idxStart] + " ");
             traverseInOrder(2*idxStart+2);
--- Pertemuan13/BinaryTreeArrayMain23.java
+++ Pertemuan13/BinaryTreeArrayMain23.java
@@ -3,12 +3,28 @@ package PraktikumAlgoritmaStrukturData.Pertemuan13;
 public class BinaryTreeArrayMain23 {
     public static void main(String[] args) {
     BinaryTreeArray23 bta = new BinaryTreeArray23();
-    int[] data = {6,4,8,3,5,7,9,0,0,0};
-    int idxLast = 6;
-    bta.populateData(data, idxLast);  
-    System.out.print("\nIn-Order Traversal  : ");
+    
+    bta.add(6);
+    bta.add(4);
+    bta.add(8);
+    bta.add(3);
+    bta.add(5);
+    bta.add(7);
+    bta.add(9);
+ 
+    
+    System.out.print("\nPre-Order Traversal     : ");
+    bta.traversePreOrder(0);
+    System.out.println("\n");
+
+    System.out.print("\nIn-Order Traversal      : ");
     bta.traverseInOrder(0);
     System.out.println("\n");
+    
+    System.out.print("\nPost-Order Traversal    : ");
+    bta.traversePostOrder(0);
+    System.out.println("\n");
+
     }
 }
 
